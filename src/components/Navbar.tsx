bcba84e7ef3c80fda933c5d7a54a1cc323bec0ba
Natan Souza <natan_gabri00@hotmail.com>
2023-11-09 20:19:11 -0300

    add tailwind and lottie on project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="w-screen border-b opacity-50 blur bg-emerald-300 border-zinc-100 text-zinc-100 flex">
+      <div>LOGO</div>
+      <div>nav</div>
+    </nav>
+  )
+}
