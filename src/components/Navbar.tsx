46f726b81e00ebed2ddab37feedd87a608a5d39b
Natan Souza <42328139+natan1506@users.noreply.github.com>
2023-11-24 14:55:09 -0300

    Feat: Add logo on project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Code } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import WriteLogo from "../assets/logo-escrita.png"
+import Logo from "../assets/logo.png"
 
 interface NavbarProps {
   scrollPosition: number
@@ -16,12 +18,12 @@ export default function Navbar({ scrollPosition }: NavbarProps) {
     if (scrollPosition > 48) {
       setStateStyle({
         navbar: "py-3 text-sm",
-        icon: "h-9 w-9"
+        icon: "h-9"
       })
     } else {
       setStateStyle({
         navbar: "py-6 text-md",
-        icon: "h-10 w-10"
+        icon: "h-10 "
       })
 
     }
@@ -30,9 +32,10 @@ export default function Navbar({ scrollPosition }: NavbarProps) {
   return (
     <nav className={[`w-full z-10 px-4 border-b border-2 text-palette-gray flex flex-1 justify-between shadow-md backdrop-blur-md fixed top-0 items-center transition-all`].join(" ")}>
       <div className={stateStyles.navbar}>
-        <Code
+        <img src={scrollPosition > 48 ? Logo : WriteLogo} alt="" className={[`transition-all`, stateStyles.icon].join(" ")} />
+        {/* <Code
           className={[`transition-all`, stateStyles.icon].join(" ")}
-        />
+        /> */}
       </div>
       <div className="border-l-2">
         <Link to="/" >
